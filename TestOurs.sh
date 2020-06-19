01e8241f8527384a86ffc20662154c36c8652ca4
wn312991 <2425176429@qq.com>
2020-06-19 21:33:11 +0800

    Add files via upload

---
+++ TestOurs.sh
@@ -0,0 +1,24 @@
+for M in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
+do
+	echo "----------------------"
+	echo " M=$M"
+	echo "----------------------"
+	##############################
+	# Run Ours.c
+	##############################
+	rm ours.out 2> /dev/null
+	
+	# calculating  b = ceiling( log2(M) )
+	if b=$(python3 b-from-M.py $M); then
+		echo "Using b=$b"
+	else
+		echo "There was error in calculation of b. Exiting..."
+		exit  2
+	fi
+	
+	gcc -Wall -I /home/wn/gsl2.4/include/ -c -O3 -D PASSED_M=$M -D PASSED_b=$b Ours.c -lm -mlzcnt -o ours.o
+	gcc -L /home/wn/gsl2.4/lib ours.o -lgsl -lgslcblas -O3 -lm -mlzcnt -o ours.out
+	./ours.out
+	
+	
+done
